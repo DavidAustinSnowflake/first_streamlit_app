cca25d1ac7e85ef16296a1f7353943d9fdc7b8ff
DavidAustinSnowflake <112190338+DavidAustinSnowflake@users.noreply.github.com>
2022-08-29 11:41:08 -0400

    Update streamlit_app.py
    Display the API data in a better way

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalize the data from the API
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display the data on the page
+streamlit.dataframe(fruityvice_normalized)
